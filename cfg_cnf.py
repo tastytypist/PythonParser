1a4edf534ffd1d4082e36bb8d22bcbcde804c551
tastytypist <nathanael310802@gmail.com>
2021-11-21 21:30:51 +0700

    Correct code attribution

--- cfg_cnf.py
+++ cfg_cnf.py
@@ -1,6 +1,7 @@
+# This file contains modified portions of code by Adel Massimo Ramadan
+
 # Copyright (c) 2018 Adel Massimo Ramadan
-# Copyright (c) 2021 tastytypist
-#
+
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
